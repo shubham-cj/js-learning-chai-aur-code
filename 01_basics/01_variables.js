185e19c8a5662817fc7d1597f4a84fa60d396392
shubham-cj <119930498+shubham-cj@users.noreply.github.com>
2023-11-30 23:12:13 +0530

    variable and data types complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,32 @@
+const accountId = 940346
+let accountEmail = "shubham@gmail.com"
+var accountPassword = "1234"
+accountCity = "Satara"
+let accountState
+
+console.log(accountState) //Undefined
+
+//accountId = 23445  //const cannot be changed
+
+accountEmail  = "djshubham@gmail.com"
+accountPassword = "4321"
+accountCity = "Koregaon"
+
+/*
+    Prefer not to use "var"
+    because of issues with block scope and functional scope
+    Instead always prefer to use "let"
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/*
+┌─────────┬───────────────────────┐
+│ (index) │        Values         │
+├─────────┼───────────────────────┤
+│    0    │        940346         │
+│    1    │ 'djshubham@gmail.com' │
+│    2    │        '4321'         │
+│    3    │      'Koregaon'       │
+│    4    │       undefined       │
+└─────────┴───────────────────────┘
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";    // treat all JS code as newer version
+
+// alert(3 + 3)   // we are using node.js, not browser
+
+// Data Types 
+
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => stand alone value and also data type
+// undefined =>
+// symbol => unique symbol
+// object => 
+console.log(typeof null);  // object
+
+console.log(typeof "Shubham");  // string
+console.log(typeof undefined);  // undefined
