80a426a7f7b8c5ce801cf04b0ee904b049c537e9
shubham-cj <119930498+shubham-cj@users.noreply.github.com>
2023-12-09 22:57:41 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,7 +8,6 @@ function sayMyName() {
     console.log("m");
     
 }
-
 // sayMyName();   // calling function
 
 
@@ -16,7 +15,6 @@ function sayMyName() {
 function addTwoNumbers(num1, num2) {      // num1 and num2 are 'parameters'
     // console.log(num1 + num2);
 }
-
 addTwoNumbers()   // NaN
 addTwoNumbers(2, 3)   // 5                // 2 and 3 are 'aruments'
 addTwoNumbers(2, "3")   // 23
@@ -26,7 +24,6 @@ addTwoNumbers(2, null)   // 2
 function addNumbers(num1, num2){
     return num1 + num2;
 }
-
 addNumbers(2, 3)    // nothing will be logged in console
 // console.log(addNumbers(2, 3));    // 5
 
@@ -37,10 +34,43 @@ function loginUserMsg(username = "user"){
     }
     return `${username} just logged in.`
 }
-
 // console.log(loginUserMsg("Shubham"));    // Shubham just logged in.
 // console.log(loginUserMsg(""));    // just logged in.
-console.log(loginUserMsg());    // user just logged in.
+// console.log(loginUserMsg());    // user just logged in.
 
 
 
+
+function calculateCartPrice(...num){        // this ... operator is called as rest operator.
+    return num
+}
+// console.log(calculateCartPrice(200, 400, 500, 2000));    // [ 200, 400, 500, 2000 ]
+
+function calculateCartPrice(val1, val2,...num){
+    return num
+}
+// console.log(calculateCartPrice(200, 400, 500, 2000));    // [ 500, 2000 ]
+
+
+
+const user = {
+    username: "shubham",
+    age: 24
+} 
+function handeleobject(obj){
+    console.log(`User is ${obj.username} and age is ${obj.age}`);
+}
+// handeleobject(user);   // User is shubham and age is 24
+handeleobject({
+    username: "shubham",
+    age: 24
+});   // User is shubham and age is 24
+
+
+
+const myNewArray = [200, 3000, 400]
+function returnSecondVAlue(array) {
+    return array[1]
+}
+// console.log(returnSecondVAlue(myNewArray));   // 3000
+console.log(returnSecondVAlue([100, 3000, 400]));   // 3000
